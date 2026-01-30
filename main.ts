ca324b6a69a3856f95b21a3e9488779b937fd249
ghwweu4e87 <Sophialiu@crosfields.com>
2026-01-30 14:30:23 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+if (input.acceleration(Dimension.X) > 15) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+}
 basic.forever(function () {
     radio.setGroup(10)
 })
